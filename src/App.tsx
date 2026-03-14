b853137157407ef35e34edc3756807be11ffb0c4
Lateema Spencer <spencer.lateema@gmail.com>
2026-03-13 22:55:24 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { Container, CssBaseline, createTheme, ThemeProvider } from "@mui/material";
+
+const darkTheme = createTheme({
+	palette: {
+		mode: "dark",
+	},
+});
+
+const App = () => {
+	return (
+		<ThemeProvider theme={darkTheme}>
+			<CssBaseline />
+			<Container>
+				<h1>Dark Mode</h1>
+			</Container>
+		</ThemeProvider>
+	);
+};
+
+export default App;
